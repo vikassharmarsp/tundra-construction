e72c5bafc11dc591e0cf8923c2d02e9f0662c094
Vikas <vikassharmajmu2@gmail.com>
2021-04-15 07:38:20 +0530

    Update

--- mailServer/contact.php
+++ mailServer/contact.php
@@ -4,10 +4,10 @@ $email_to = 'vikassharmajmu2@gmail.com';
 
 if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])){   
     $host = "relay-hosting.secureserver.net";
-    $port = "25";
+    $port = "26";
     $name = $_POST['name'];
     $email = $_POST['email']; 
-    $phone = $_POST['phone'];
+    // $phone = $_POST['phone'];
     $message = $_POST['message'];
 
     $headers = "From: $email"; 
@@ -15,18 +15,20 @@ if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message']
 
     $msg = "Sender Name:\t$name\n"; 
     $msg .= "Sender E-Mail:\t$email\n"; 
-    $msg .= "Message Phone:\t$phone\n"; 
+    // $msg .= "Message Phone:\t$phone\n"; 
     $msg .= "Message:\t$message\n\n"; 
 
     $sent = mail($email_to, $subject, $msg, $headers) ; 
 
     if($sent) {
-        //alert("mail sent");
-header("Location : https://tundra-construction.herokuapp.com");
-
+        header("location: http://www.tundra-construction.herokuapp.com");
+        exit();
     } else {
-        //alert("mail not sent");
+        header("location: http://www.tundra-construction.herokuapp.com/contact.html");
+        exit();
     }
 }else{
-    
+    header("location: http://www.tundra-construction.herokuapp.com/about.html");
+    exit();
 }
+?>
